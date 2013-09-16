666876935121d101f6531ab791811c70a8e7bd80
sa_ddam213 <sa_ddam213@live.com>
2013-09-16 11:33:26 +0000

    Progress value fixed

---
+++ GUIFramework/Converters/ProgressValueConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using GUISkinFramework;
+using System.Windows;
+
+namespace GUIFramework.Converters
+{
+  
+    public class ProgressValueConverter : IMultiValueConverter
+    {
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Count() == 3)
+            {
+                try
+                {
+                    double percentage = (double)values[1];
+                    var width = values[0] != null ? double.Parse(values[0].ToString()) : 0.0;
+                    var margin = values[2] != null ? values[2].ToString().ToThickness() : new Thickness(0);
+                    var actualWidth = (width - (margin.Left + margin.Right));
+                    return Math.Min(actualWidth, Math.Max(0.0, (actualWidth / 100.0) * percentage));
+                }
+                catch { }
+            }
+            return 0.0;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- GUIFramework/GUI/Controls/GUIProgressBar.xaml.cs
+++ GUIFramework/GUI/Controls/GUIProgressBar.xaml.cs
@@ -17,6 +17,8 @@ using GUISkinFramework.Controls;
 using GUIFramework.Managers;
 using System.Collections.ObjectModel;
 using GUISkinFramework.Property;
+using System.Globalization;
+using GUISkinFramework;
 
 namespace GUIFramework.GUI.Controls
 {
@@ -26,55 +28,96 @@ namespace GUIFramework.GUI.Controls
     [XmlSkinType(typeof(XmlProgressBar))]  
     public partial class GUIProgressBar : GUIControl
     {
-        private int _progress;
-     
+        private double _progress;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GUIProgressBar"/> class.
+        /// </summary>
         public GUIProgressBar()
         {
             InitializeComponent(); 
         }
 
+        /// <summary>
+        /// Gets the skin XML.
+        /// </summary>
         public XmlProgressBar SkinXml
         {
             get { return BaseXml as XmlProgressBar; }
         }
 
-        public int Progress
+        /// <summary>
+        /// Gets or sets the progress.
+        /// </summary>
+        public double Progress
         {
             get { return _progress; }
-            set { _progress = value; NotifyPropertyChanged("Progress"); }
+            set
+            {
+                if (HasChanged(_progress, value))
+                {
+                    _progress = value;
+                    NotifyPropertyChanged("Progress");
+                }
+            }
         }
 
-    
-
+        /// <summary>
+        /// Creates the control.
+        /// </summary>
         public override void CreateControl()
         {
             base.CreateControl();
             RegisteredProperties = PropertyRepository.GetRegisteredProperties(this, SkinXml.ProgressValue);
         }
 
+        /// <summary>
+        /// Registers the info data.
+        /// </summary>
         public override void RegisterInfoData()
         {
             base.RegisterInfoData();
             PropertyRepository.RegisterPropertyMessage(this, SkinXml.ProgressValue);
         }
 
+        /// <summary>
+        /// Deregisters the info data.
+        /// </summary>
         public override void DeregisterInfoData()
         {
             base.DeregisterInfoData();
             PropertyRepository.DeregisterPropertyMessage(this, SkinXml.ProgressValue);
         }
 
+        /// <summary>
+        /// Updates the info data.
+        /// </summary>
         public async override void UpdateInfoData()
         {
             base.UpdateInfoData();
-            Progress = await PropertyRepository.GetProperty<int>(SkinXml.ProgressValue);
+            Progress = await PropertyRepository.GetProperty<double>(SkinXml.ProgressValue);
         }
 
+        /// <summary>
+        /// Clears the info data.
+        /// </summary>
         public override void ClearInfoData()
         {
             base.ClearInfoData();
             Progress = 0;
         }
+
+        /// <summary>
+        /// Determines whether the specified value has changed.
+        /// </summary>
+        /// <param name="value1">The value1.</param>
+        /// <param name="value2">The value2.</param>
+        /// <returns>If the value has changed to 1 decmal point</returns>
+        private bool HasChanged(double value1, double value2)
+        {
+            return Math.Round(value1, 1) != Math.Round(value2, 1);
+        }
     }
+
+ 
 }
